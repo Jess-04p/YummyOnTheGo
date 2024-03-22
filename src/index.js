b053d4ce164ced19e9f772b25f56bc1358cf483b
keer918 <135299700+keer918@users.noreply.github.com>
2024-03-22 06:52:08 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>HELLO JESS</h1>
+    <h1>HELLO keer</h1>
   </React.StrictMode>
 );
 
